d225c376ec31bfc3c3829aa2938acf5c1279eb42
Astrospence <illustrator.wood@gmail.com>
2021-08-17 13:45:59 -0600

    finished task 3

--- index.js
+++ index.js
@@ -43,10 +43,13 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
-    /* code here */
+function getYears(data, getFinalscb) {
+    const years = getFinalscb(data).map(function(item){
+        return item.Year
+    })
+    return years;
 }
-
+console.log('Task 3:', getYears(fifaData, getFinals));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
